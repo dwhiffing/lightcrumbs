0c3ec73287ccb8a1d8fd468c1c86c10f37c4047c
Daniel Whiffing <danielwhiffing@gmail.com>
2023-05-28 19:20:19 -0400

    look more central vertically

--- src/services/InputService.ts
+++ src/services/InputService.ts
@@ -160,7 +160,7 @@ export class InputService {
     theta -= dx * (0.25 / 2)
     theta %= 360
     phi += dy * (-0.25 / 2)
-    phi = Math.min(15, Math.max(-15, phi))
+    phi = 5
 
     const lookAt = new THREE.Vector3()
     lookAt.x =
@@ -177,4 +177,4 @@ export class InputService {
 }
 
 let theta = 0
-let phi = 0
+let phi = 5
