3426c78f28dffcec6875671452f97cf5f06c504e
Daniel Whiffing <danielwhiffing@gmail.com>
2023-05-28 15:39:41 -0400

    3 levels for now

--- src/maps/index.ts
+++ src/maps/index.ts
@@ -1,7 +1,7 @@
 import map1 from './map1.json'
 import map2 from './map2.json'
 import map3 from './map3.json'
-import map4 from './map4.json'
-import map5 from './map5.json'
+// import map4 from './map4.json'
+// import map5 from './map5.json'
 
-export const MAPS = [map1, map2, map3, map4, map5]
+export const MAPS = [map1, map2, map3]
